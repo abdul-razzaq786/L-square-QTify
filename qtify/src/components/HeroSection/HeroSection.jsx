49eec30991b4ca60a73db377d01bd7511cc3c248
abdul razzaq <abdulrazzaq@gmail.com>
2024-07-24 23:46:43 +0530

    commit

--- qtify/src/components/HeroSection/HeroSection.jsx
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -1,22 +1,22 @@
-// /* eslint-disable jsx-a11y/img-redundant-alt */
-// import React from 'react'
-// import styles from './HeroSection.module.css'
-// import heroImage from "../../assets/heroImage.png"
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react'
+import styles from './HeroSection.module.css'
+import heroImage from "../../assets/heroImage.png"
 
 
-// const HeroSection =()=> {
-//   return (
-//     <section className={styles.heroSection}>
-//         <div className={styles.container}>
-//             <div className={styles.heroText}>
-//             <h1>100 Thousand Songs, ad-free</h1> 
-//             <h1>and Over thousands podcast episodes</h1>
-//             </div>
-//             <div className={styles.heroImage}>
-//             <img src={ heroImage } alt="hero-image" height="200" width="200" loading='eager' />
-//             </div>
-//         </div>
-//     </section>
-//   )
-// }
-// export default HeroSection
+const HeroSection =()=> {
+  return (
+    <section className={styles.heroSection}>
+        <div className={styles.container}>
+            <div className={styles.heroText}>
+            <h1>100 Thousand Songs, ad-free</h1> 
+            <h1>and Over thousands podcast episodes</h1>
+            </div>
+            <div className={styles.heroImage}>
+            <img src={ heroImage } alt="hero-image" height="200" width="200" loading='eager' />
+            </div>
+        </div>
+    </section>
+  )
+}
+export default HeroSection
