8ed4cd4996e0261f20244f7ce7ef0fd2db8d6aa1
abdul razzaq <abdulrazzaq@gmail.com>
2024-07-26 23:03:57 +0530

    commit

--- qtify/src/index.js
+++ qtify/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import HomePage from "../src/pages/HomePage/HomePage.jsx";
+import HomePage from "../src/pages/HomePage/HomePage.jsx";
 // import AlbumPage from "../src/pages/AlbumPage/AlbumPage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -10,10 +10,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    // // children: [
-    // //   { path: "/", element: <HomePage /> },
+    children: [
+      { path: "/", element: <HomePage /> },
     //   // { path: "/album/:albumId", element: <AlbumPage /> },
-    // ],
+    ],
   },
 ]);
 
--- qtify/src/pages/HomePage/HomePage.jsx
+++ qtify/src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,27 @@
-// import HeroSection from "../../components/HeroSection/HeroSection";
+import HeroSection from "../../components/HeroSection/HeroSection";
 // import { useOutletContext } from "react-router-dom";
 // import CardCarousel from "../../components/CardCarousel/CardCarousel";
 // import { fetchFilters } from "../../api/api";
 // import styles from "./HomePage.module.css";
-// const HomePage = () => {
+const HomePage = () => {
 //   const { data } = useOutletContext();
 //   const { topAlbums = [], newAlbums = [], songs = [] } = data;
 
-//   return (
-//     <>
-//       <HeroSection />
-//       <div className={styles.cardSection}>
-//         <CardCarousel title="Top Albums" data={topAlbums} type="album" />
-//         <CardCarousel title="New Albums" data={newAlbums} type="album" />
-//         <CardCarousel
-//           title="Songs"
-//           filterSource={fetchFilters}
-//           data={songs}
-//           type="songs"
-//         />
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <HeroSection />
+      {/* <div className={styles.cardSection}>
+        <CardCarousel title="Top Albums" data={topAlbums} type="album" />
+        <CardCarousel title="New Albums" data={newAlbums} type="album" />
+        <CardCarousel
+          title="Songs"
+          filterSource={fetchFilters}
+          data={songs}
+          type="songs"
+        />
+      </div> */}
+    </>
+  );
+};
 
-// export default HomePage;
+export default HomePage;
